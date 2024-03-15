7298d099bf3c0afd0b10aa0492bc54372be2776d
VisionovaDev <dev@visionova.it>
2024-03-15 13:01:23 +0100

    Installazione boostrap UI Auth

--- local-volumes/nginx/www/vite.config.js
+++ local-volumes/nginx/www/vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        hmr: {
+        host: '127.0.0.1',
+        port: 5173,
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
