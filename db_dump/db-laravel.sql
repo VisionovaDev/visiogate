3f85e1542922ad3d7c4e8202dadcd83f872928ed
VisionovaDev <dev@visionova.it>
2024-03-15 17:57:03 +0100

    Esportato DB

--- db_dump/db-laravel.sql
+++ db_dump/db-laravel.sql
@@ -19,8 +19,35 @@
 CREATE DATABASE IF NOT EXISTS `laravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `laravel`;
 
+-- Dump della struttura di tabella laravel.aziende
+CREATE TABLE IF NOT EXISTS `aziende` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `indirizzo` varchar(255) DEFAULT NULL,
+  `citta` varchar(255) DEFAULT NULL,
+  `cap` varchar(255) DEFAULT NULL,
+  `provincia` varchar(2) DEFAULT NULL,
+  `nazione` varchar(255) DEFAULT NULL,
+  `sito` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `privacy_it` varchar(255) DEFAULT NULL,
+  `privacy_en` varchar(255) DEFAULT NULL,
+  `privacy_de` varchar(255) DEFAULT NULL,
+  `privacy_fr` varchar(255) DEFAULT NULL,
+  `privacy_es` varchar(255) DEFAULT NULL,
+  `logo_file_path` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dump dei dati della tabella laravel.aziende: ~1 rows (circa)
+DELETE FROM `aziende`;
+INSERT INTO `aziende` (`id`, `nome`, `indirizzo`, `citta`, `cap`, `provincia`, `nazione`, `sito`, `email`, `telefono`, `privacy_it`, `privacy_en`, `privacy_de`, `privacy_fr`, `privacy_es`, `logo_file_path`, `created_at`, `updated_at`) VALUES
+	(1, 'Ragione sociale', 'via ....', 'Brescia', '25125', 'BS', 'Italia', 'https://www.nomedelsito.it', 'email@emaildaimpostare.ko', '0000000', NULL, NULL, NULL, NULL, NULL, '/storage/images/logo.png', '2024-03-15 16:39:36', '2024-03-15 16:39:36');
+
 -- Dump della struttura di tabella laravel.cache
-DROP TABLE IF EXISTS `cache`;
 CREATE TABLE IF NOT EXISTS `cache` (
   `key` varchar(255) NOT NULL,
   `value` mediumtext NOT NULL,
@@ -32,7 +59,6 @@ CREATE TABLE IF NOT EXISTS `cache` (
 DELETE FROM `cache`;
 
 -- Dump della struttura di tabella laravel.cache_locks
-DROP TABLE IF EXISTS `cache_locks`;
 CREATE TABLE IF NOT EXISTS `cache_locks` (
   `key` varchar(255) NOT NULL,
   `owner` varchar(255) NOT NULL,
@@ -44,7 +70,6 @@ CREATE TABLE IF NOT EXISTS `cache_locks` (
 DELETE FROM `cache_locks`;
 
 -- Dump della struttura di tabella laravel.failed_jobs
-DROP TABLE IF EXISTS `failed_jobs`;
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(255) NOT NULL,
@@ -61,7 +86,6 @@ CREATE TABLE IF NOT EXISTS `failed_jobs` (
 DELETE FROM `failed_jobs`;
 
 -- Dump della struttura di tabella laravel.jobs
-DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE IF NOT EXISTS `jobs` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `queue` varchar(255) NOT NULL,
@@ -78,7 +102,6 @@ CREATE TABLE IF NOT EXISTS `jobs` (
 DELETE FROM `jobs`;
 
 -- Dump della struttura di tabella laravel.job_batches
-DROP TABLE IF EXISTS `job_batches`;
 CREATE TABLE IF NOT EXISTS `job_batches` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -97,25 +120,26 @@ CREATE TABLE IF NOT EXISTS `job_batches` (
 DELETE FROM `job_batches`;
 
 -- Dump della struttura di tabella laravel.migrations
-DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dump dei dati della tabella laravel.migrations: ~5 rows (circa)
+-- Dump dei dati della tabella laravel.migrations: ~8 rows (circa)
 DELETE FROM `migrations`;
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(5, '0001_01_01_000000_create_users_table', 1),
 	(6, '0001_01_01_000001_create_cache_table', 1),
 	(7, '0001_01_01_000002_create_jobs_table', 1),
 	(8, '2024_03_15_103639_add_deleted_at_to_users_table', 1),
-	(9, '2024_03_15_101731_create_permission_tables', 2);
+	(9, '2024_03_15_101731_create_permission_tables', 2),
+	(36, '2024_03_15_134105_create_aziende_table', 3),
+	(37, '2024_03_15_144412_create_sedi_table', 3),
+	(38, '2024_03_15_150913_create_varchi_table', 3);
 
 -- Dump della struttura di tabella laravel.model_has_permissions
-DROP TABLE IF EXISTS `model_has_permissions`;
 CREATE TABLE IF NOT EXISTS `model_has_permissions` (
   `permission_id` bigint(20) unsigned NOT NULL,
   `model_type` varchar(255) NOT NULL,
@@ -129,7 +153,6 @@ CREATE TABLE IF NOT EXISTS `model_has_permissions` (
 DELETE FROM `model_has_permissions`;
 
 -- Dump della struttura di tabella laravel.model_has_roles
-DROP TABLE IF EXISTS `model_has_roles`;
 CREATE TABLE IF NOT EXISTS `model_has_roles` (
   `role_id` bigint(20) unsigned NOT NULL,
   `model_type` varchar(255) NOT NULL,
@@ -143,7 +166,6 @@ CREATE TABLE IF NOT EXISTS `model_has_roles` (
 DELETE FROM `model_has_roles`;
 
 -- Dump della struttura di tabella laravel.password_reset_tokens
-DROP TABLE IF EXISTS `password_reset_tokens`;
 CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
@@ -155,7 +177,6 @@ CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
 DELETE FROM `password_reset_tokens`;
 
 -- Dump della struttura di tabella laravel.permissions
-DROP TABLE IF EXISTS `permissions`;
 CREATE TABLE IF NOT EXISTS `permissions` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -170,7 +191,6 @@ CREATE TABLE IF NOT EXISTS `permissions` (
 DELETE FROM `permissions`;
 
 -- Dump della struttura di tabella laravel.roles
-DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -185,7 +205,6 @@ CREATE TABLE IF NOT EXISTS `roles` (
 DELETE FROM `roles`;
 
 -- Dump della struttura di tabella laravel.role_has_permissions
-DROP TABLE IF EXISTS `role_has_permissions`;
 CREATE TABLE IF NOT EXISTS `role_has_permissions` (
   `permission_id` bigint(20) unsigned NOT NULL,
   `role_id` bigint(20) unsigned NOT NULL,
@@ -198,8 +217,30 @@ CREATE TABLE IF NOT EXISTS `role_has_permissions` (
 -- Dump dei dati della tabella laravel.role_has_permissions: ~0 rows (circa)
 DELETE FROM `role_has_permissions`;
 
+-- Dump della struttura di tabella laravel.sedi
+CREATE TABLE IF NOT EXISTS `sedi` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `indirizzo` varchar(255) DEFAULT NULL,
+  `citta` varchar(255) DEFAULT NULL,
+  `cap` varchar(255) DEFAULT NULL,
+  `provincia` varchar(2) DEFAULT NULL,
+  `nazione` varchar(255) DEFAULT NULL,
+  `sito` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dump dei dati della tabella laravel.sedi: ~1 rows (circa)
+DELETE FROM `sedi`;
+INSERT INTO `sedi` (`id`, `nome`, `indirizzo`, `citta`, `cap`, `provincia`, `nazione`, `sito`, `email`, `telefono`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Sede unica', 'via ....', 'Brescia', '25125', 'BS', 'Italia', 'https://www.nomedelsito.it', 'email@emaildaimpostare.ko', '0000000', '2024-03-15 16:39:36', '2024-03-15 16:39:36', NULL);
+
 -- Dump della struttura di tabella laravel.sessions
-DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE IF NOT EXISTS `sessions` (
   `id` varchar(255) NOT NULL,
   `user_id` bigint(20) unsigned DEFAULT NULL,
@@ -212,13 +253,13 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   KEY `sessions_last_activity_index` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dump dei dati della tabella laravel.sessions: ~1 rows (circa)
+-- Dump dei dati della tabella laravel.sessions: ~2 rows (circa)
 DELETE FROM `sessions`;
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-	('8bVCoi0PaIDQ3CKlEBNpjPJN8I9uGU0lztS90MQz', NULL, '172.18.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiR1N5Y281R2x5b016RFdEWmkwcURrdjlxT0dBY2JydlVXQnNGSUlDQyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MTY6Imh0dHA6Ly9sb2NhbGhvc3QiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1710497619);
+	('xWPyoLmPRv169YRanvy3anHI0dXHzHf7nEy2fByX', NULL, '172.26.0.1', 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiczZRUENsVnJaNnRFc0xwQ1hKbHp4OG5sM1R0UlFnSVgxUEZoMGRjTyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MTY6Imh0dHA6Ly9sb2NhbGhvc3QiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1710520442),
+	('ZrhPmoNvIKjrLYhX2D1rt5Tx4AdPX6ZXZwhHpdNE', 1, '172.26.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoidVhXcmxVbGROVVVVbWRmbWgxckxpT3EyazI2dThzYWtMQnhLQlNFOCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MTY6Imh0dHA6Ly9sb2NhbGhvc3QiO31zOjM6InVybCI7YTowOnt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjQ6ImF1dGgiO2E6MTp7czoyMToicGFzc3dvcmRfY29uZmlybWVkX2F0IjtpOjE3MTA1MDc1ODg7fX0=', 1710509394);
 
 -- Dump della struttura di tabella laravel.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -233,10 +274,30 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dump dei dati della tabella laravel.users: ~1 rows (circa)
+-- Dump dei dati della tabella laravel.users: ~0 rows (circa)
 DELETE FROM `users`;
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 'Admin', 'webmaster@visionova.it', NULL, '$2y$12$Cw6vD2.mQQ2HxRB5e4wCa.OtCD5dAipP8vZ/qnEEGkBGFHqndZi8e', NULL, '2024-03-15 10:04:24', '2024-03-15 10:04:24', NULL);
+
+-- Dump della struttura di tabella laravel.varchi
+CREATE TABLE IF NOT EXISTS `varchi` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sedi_id` bigint(20) unsigned NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `is_ingresso` tinyint(1) NOT NULL DEFAULT 1,
+  `is_uscita` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `varchi_sedi_id_foreign` (`sedi_id`),
+  CONSTRAINT `varchi_sedi_id_foreign` FOREIGN KEY (`sedi_id`) REFERENCES `sedi` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dump dei dati della tabella laravel.varchi: ~1 rows (circa)
+DELETE FROM `varchi`;
+INSERT INTO `varchi` (`id`, `sedi_id`, `nome`, `is_ingresso`, `is_uscita`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 1, 'Ingresso principale', 1, 1, '2024-03-15 16:39:36', '2024-03-15 16:39:36', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
